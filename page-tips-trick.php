8c4f022356880cd30c45128a34ac6a025011d982
Newbo94 <jannilas@hotmail.de>
2017-11-28 09:49:44 +0100

    Tips & ticks, Main css

--- page-tips-trick.php
+++ page-tips-trick.php
@@ -6,27 +6,57 @@
 
  ?>
 
-<?php get_header();?>
+<?php get_header(); ?>
 
 
-<?php
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
+<!-- Newsletter-->
+
+<style>
+
+.news-bg {
+  background-image: url("<?php bloginfo('template_directory'); ?>/image/newsletter-bg.png");
+  background-position: center;
+  background-repeat: no-repeat;
+background-size: cover;
+height: 450px;
+justify-content: center;
+    display: flex;
+    align-items: center;
+}
 
 
 
-		//
-    $terms = get_terms([
-        'taxonomy' => $taxonomy,
-        'hide_empty' => false,
-    ]);
+</style>
 
-		//
-	} // end while
-} // end if
-?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+
+      <ul><?php echo get_the_term_list( $post->ID, 'mycategories', '<li class="jobs_item">', ', ', '</li>' ) ?></ul>
+
+
+    </div>
+  </div>
+</div>
+
+
+<div class="container-fluid news-bg">
+  <div class="row">
+    <div class="col-lg-12">
+      <?php if ( have_posts() ) : ?>
+      	<?php while ( have_posts() ) : the_post(); ?>
+
+          <div class="top-offset-50">
+          <?php the_content(); ?>
+          </div>
+      	<!-- do stuff ... -->
+      	<?php endwhile; ?>
+      <?php endif; ?>
+
+    </div>
+  </div>
+</div>
 
 
 
