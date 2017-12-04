e0d5e06b3fb8d13e910d1d21f4c4d114065ced02
Newbo94 <jannilas@hotmail.de>
2017-12-04 12:23:25 +0100

    Bg HER ANDY OG MICHELLE

--- index.php
+++ index.php
@@ -20,36 +20,91 @@ get_header(); ?>
 
 
 <div id="primary" class="content-area container top-offset-50">
-	<main id="main" class="site-main row">
+	<main id="main" class="site-main ">
+
+
+		<header class="page-header">
+			<span class="overline"></span>
+
+				<h1>Bloggen for jobsøgende</h1>
 
 
 
 
+<div class="row">
+
+	<div class="cat-link">
 		<?php
-		if ( have_posts() ) :
+	$categories = get_categories( array(
+	    'orderby' => 'name',
+	    'order'   => 'ASC'
+	) );
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+	foreach( $categories as $category ) {
+	    $category_link = sprintf(
+	        '<a href="%1$s" alt="%2$s">%3$s</a>',
+	        esc_url( get_category_link( $category->term_id ) ),
+	        esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
+	        esc_html( $category->name )
+	    );
 
-<?php endif; ?>
-<?php endif; ?>
+	    echo '<p>' . sprintf( esc_html__( 'Kategorier: %s', 'textdomain' ), $category_link ) . '</p> ';
+
+	}
+		?>
+	</div>
+
+</div>
 
 
-				<?php if ( have_posts() ) : ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-					<?php 			get_template_part( 'template-parts/content-card', get_post_format() );
- ?>
+
+
+ <!-- YOAST SEO breadcrumbs -->
+
+		<div class="row">
+			<?php
+			if ( function_exists('yoast_breadcrumb') ) {
+			yoast_breadcrumb('
+			<p id="breadcrumbs">','</p>
+			');
+			}
+			?>
+		</div>
+
+
+</header>
+
+
+
+
+			<div class="row">
+
+
+
+
+
+				<?php
+				$args = array( 'post_type' => 'post', 'posts_per_page' => -1 );
+				$the_query = new WP_Query( $args );
+				?>
+				<?php if ( $the_query->have_posts() ) : ?>
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+
+						<?php 			get_template_part( 'template-parts/content-post', get_post_format() );
+			?>
+
+
+						<?php wp_reset_postdata(); ?>
 					<?php endwhile; ?>
 				<?php endif; ?>
-
 
 
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+
 <?php
-get_sidebar();
-get_footer();
+ get_footer();
+ get_sidebar();
